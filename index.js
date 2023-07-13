412026465d2c6fe3de776da1ed75c1846d61401c
johnkagiri <kagirijohn124@gmail.com>
2023-07-13 10:17:23 +0300

    Completed asssignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(name,message){
+    var v=[]
+    for (var i=0;i<name.length;i++){
+      v.push(`Thank you, ${name[i]}, for the wonderful ${message} gift!`)
+    }return v
+  }
+  function countDown(number){
+    for(var i=number; i>=0; i--){
+        console.log(i);
+    }
+    
+  }
+countDown(10);
